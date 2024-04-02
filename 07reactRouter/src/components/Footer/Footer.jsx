6670fb542cbfab26bef561f6b7f8e7390098eaf1
ESHUshri202 <eshushrivastava1106@gmail.com>
2024-04-02 12:44:27 +0530

    ConteztApi

--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -72,7 +72,7 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2024 
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
+                        <a href="https://hiteshchoudhary.com/" className="hover:underline">.
                              Achintya 
                         </a>
                         . All Rights Reserved.
